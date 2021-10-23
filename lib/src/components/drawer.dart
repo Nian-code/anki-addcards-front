1010bc7133f46d7bc9542f0cd18d15a9555f5593
Nicoll Angulo <nicoll.mejia@mercadolibre.com.co>
2021-10-22 22:25:56 -0500

    add button settings

--- lib/src/components/drawer.dart
+++ lib/src/components/drawer.dart
@@ -1,17 +1,44 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:anki_addcards_front/src/ui/themes/themes.dart';
 import 'package:flutter/material.dart';
 
 Widget drawerPage(){
     return Drawer(
-    child: ListView(
+    child:  
+      ListView(
       children: [
-        Container(
-        color: currentTheme.getPrimaryColor(),
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
+        _getHeader(),
+        ListTile(
+          title: Text("Settings"),
+          leading: Icon(Icons.settings),
+          onTap: (){
+            print("object");
+          },
+        )
+          ],
+    ),
+    );
+  }
+
+Container _getHeader() {
+  return Container(
+      color: currentTheme.getPrimaryColor(),
+      child:
+        Padding(
+          padding: const EdgeInsets.all(8.0),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
+              SizedBox(width: 12,),
+              const Text("Hello",
+                style: 
+                  TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500
+                  ),
+              ),
+              Expanded(child: SizedBox()),
               IconButton(
                   iconSize: 28,
                   icon: Icon(
@@ -22,18 +49,8 @@ Widget drawerPage(){
                     currentTheme.switchTheme();
                   },
                 ),
-              const Text("Hello",
-                style: 
-                  TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w500
-                  ),
-              ),
               ],
-              ),
+            ),
         ),
-            height: 70,)
-          ],
-    ),
-    );
-  }
+          height: 70,);
+}
